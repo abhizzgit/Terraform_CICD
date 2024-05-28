d2a40b84ca187e6d104f8ea315621d738267c814
abhizzgit <168184232+abhizzgit@users.noreply.github.com>
2024-05-28 15:21:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "dev" {
+resource "aws_instance" "ashas" {
     ami = "ami-0cc9838aa7ab1dce7"
-    instance_type = "t2.nano"
+    instance_type = "t2.micro"
     tags = {
-      Name = "dev-ec2"
+      Name = "abhi"
     }
 }
